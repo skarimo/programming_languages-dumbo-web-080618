addfdde7e9a572bec3609b6bfa9f97e204ae4abc
Sherzod Karimov <karimovj@gmail.com>
2018-07-24 03:31:58 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,10 +4,13 @@ def reformat_languages(languages)
 new_hash = {}
  languages.each do |k, v|
   v.each do |key, value|
+   if new_hash.keys.to_s.include?(key.to_s)
+     
+    else
     new_hash[key] = {}
     new_hash[key] = value
         new_hash[key][:style] = [k]
-        binding.pry
+        
 end
 end
 new_hash
